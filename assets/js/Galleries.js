2a3f305502cc059a41395eac1472abefc0d31bbc
DamsFX <contact@hounddd.fr>
2023-05-25 15:17:53 +0200

    Bugfix Sortable script call to null elements

--- assets/js/Galleries.js
+++ assets/js/Galleries.js
@@ -2,23 +2,24 @@ function initializeSorting() {
 
     var $elementRows = $(".drag-handle").parents('table.data tbody');
 
-    Sortable.create($elementRows[0], {
-		handle: '.drag-handle',
-		animation: 150,
-		onEnd: function (evt) {
-			var $inputs = $(evt.srcElement).find('td>div.drag-handle>input');
-			var $form = $('<form style="display: none;">');
-			$form
-                .append($inputs.clone())
-                .request('onReorderRelation', {
-					complete: function () {
-                        $form.remove();
-                        initializeSorting();
-					}
-				});
-		}
-    });
-
+    if ($elementRows.length > 0) {
+        Sortable.create($elementRows[0], {
+            handle: '.drag-handle',
+            animation: 150,
+            onEnd: function (evt) {
+                var $inputs = $(evt.srcElement).find('td>div.drag-handle>input');
+                var $form = $('<form style="display: none;">');
+                $form
+                    .append($inputs.clone())
+                    .request('onReorderRelation', {
+                        complete: function () {
+                            $form.remove();
+                            initializeSorting();
+                        }
+                    });
+            }
+        });
+    }
 }
 
 $(function () {
